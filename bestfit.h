4b2f47573f27ae6383af8d48a6dc5893b928984c
Mateus Antônio Pereira Gomes <mateus_antonio3@hotmail.com>
2018-10-22 02:03:13 -0300

    Memory leaks and memory errors fixed

---
+++ bestfit.h
@@ -0,0 +1,8 @@
+#ifndef BESTFIT_H
+#define BESTFIT_H
+
+#include "PQ.h"
+
+void bestfit(PQ* pq,Item fileSize); //Fits a file into a partition or creates another one.
+
+#endif //BESTFIT_H
--- main.c
+++ main.c
@@ -2,25 +2,27 @@
 #include "PQ.h"
 #include "worstfit.h"
 
-PQ* file_read(char const *filename, int* numberOfFiles);
+PQ* file_read(char const *filename);
 
 int main(int argc, char const *argv[])
 {
-  int *numberOfFiles;
-  PQ* pq = file_read(argv[1], numberOfFiles);
+  PQ* pq = file_read(argv[1]);
   PQ_print(pq);
   printf("Quantidade de discos: %d\n", PQ_size(pq));
+  PQ_finish(pq);
   return 0;
 }
 
-PQ* file_read(char const *filename, int* numberOfFiles){
+PQ* file_read(char const *filename){
   FILE* fp = fopen(filename, "r");
-  fscanf(fp,"%d", numberOfFiles);
-  PQ *pq = PQ_init(*numberOfFiles);
-  int fileSize;
+  int numberOfFiles;
+  fscanf(fp,"%d", &numberOfFiles);
+  PQ *pq = PQ_init(numberOfFiles);
+  int fileSize = 0;
   while((fscanf(fp, "%d", &fileSize))!= EOF){
     // printf("%d\n", fileSize);
     worstfit(pq, fileSize);
   }
+  fclose(fp);
   return pq;
 }
